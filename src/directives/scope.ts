3d3ae3e7a6ee2754758009bc9df6596e27875955
Evan You <yyx990803@gmail.com>
2021-07-03 14:58:17 -0400

    supported nested scope refs

--- src/directives/scope.ts
+++ src/directives/scope.ts
@@ -1,27 +1,24 @@
 import { reactive } from '@vue/reactivity'
-import { isObject } from '@vue/shared'
 import { Context } from '../walk'
 
-export const createScopedContext = (ctx: Context, data: object): Context => {
-  if (isObject(data)) {
-    const parentScope = ctx.scope
-    const mergedScope = Object.create(parentScope)
-    Object.defineProperties(mergedScope, Object.getOwnPropertyDescriptors(data))
-    const proxy = new Proxy(mergedScope, {
-      set(target, key, val, receiver) {
-        // when setting a property that doesn't exist on current scope,
-        // do not create it on the current scope and fallback to parent scope.
-        if (receiver === reactiveProxy && !target.hasOwnProperty(key)) {
-          return Reflect.set(parentScope, key, val)
-        }
-        return Reflect.set(target, key, val, receiver)
+export const createScopedContext = (ctx: Context, data = {}): Context => {
+  const parentScope = ctx.scope
+  const mergedScope = Object.create(parentScope)
+  Object.defineProperties(mergedScope, Object.getOwnPropertyDescriptors(data))
+  mergedScope.$refs = Object.create(parentScope.$refs)
+  const proxy = new Proxy(mergedScope, {
+    set(target, key, val, receiver) {
+      // when setting a property that doesn't exist on current scope,
+      // do not create it on the current scope and fallback to parent scope.
+      if (receiver === reactiveProxy && !target.hasOwnProperty(key)) {
+        return Reflect.set(parentScope, key, val)
       }
-    })
-    const reactiveProxy = reactive(proxy)
-    return {
-      ...ctx,
-      scope: reactiveProxy
+      return Reflect.set(target, key, val, receiver)
     }
+  })
+  const reactiveProxy = reactive(proxy)
+  return {
+    ...ctx,
+    scope: reactiveProxy
   }
-  return ctx
 }
