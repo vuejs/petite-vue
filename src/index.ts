3af334c63f083956d438e316faa5dcb3c2e236a3
bran.nie <pengcheng1995@gmail.com>
2022-02-08 23:07:35 +0800

    feat: add version display

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { createApp } from './app'
 export { nextTick } from './scheduler'
 export { reactive } from '@vue/reactivity'
+export { version } from '../package.json'
 
 import { createApp } from './app'
 
