54fcf3c66cc38da5f047fd37accbab343cb54b2f
lwx <weixiang.l@outlook.com>
2023-09-18 09:49:27 +0800

    v-html.script, find script and to execute

--- src/directives/html.ts
+++ src/directives/html.ts
@@ -1,7 +1,21 @@
+import { nextTick } from '../scheduler'
 import { Directive } from '.'
 
-export const html: Directive = ({ el, get, effect }) => {
+export const html: Directive = ({ el, modifiers, get, effect }) => {
   effect(() => {
     el.innerHTML = get()
+    if (modifiers?.script) {
+      nextTick(() => {
+        const cs = el.querySelectorAll('script')
+        for (const oldScript of cs) {
+          const newScript = document.createElement('script')
+          oldScript.type && (newScript.type = oldScript.type)
+          oldScript.src && (newScript.src = oldScript.src)
+          oldScript.text && (newScript.text = oldScript.text)
+          oldScript.parentElement?.insertBefore(newScript, oldScript)
+          oldScript.remove()
+        }
+      })
+    }
   })
 }
